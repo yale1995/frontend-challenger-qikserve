2d980c59d2dcce8b8a88e5e13d2c7c70e5a81393
Yale Araujo <yale850@gmail.com>
2024-09-14 15:42:12 -0300

    refactor: new types added

--- src/@types/api-type.d.ts
+++ src/@types/api-type.d.ts
@@ -31,4 +31,62 @@ interface Venue {
   currency: string
 }
 
-export { Venue, WebSettings }
+interface ModifierItem {
+  id: number
+  name: string
+  price: number
+  maxChoices: number
+  position: number
+  visible: number
+  availabilityType: string
+  available: boolean
+  qty?: number
+}
+
+interface Image {
+  id: number
+  image: string
+}
+
+interface Modifier {
+  id: number
+  name: string
+  minChoices: number
+  maxChoices: number
+  items: ModifierItem[]
+}
+
+interface MenuItem {
+  id: number
+  name: string
+  description?: string
+  alcoholic: number
+  price: number
+  position: number
+  visible?: number
+  availabilityType: string
+  sku: string
+  images?: Image[]
+  available: boolean
+  modifiers?: Modifier[]
+}
+
+interface Section {
+  id: number
+  name: string
+  description?: string | null
+  position: number
+  visible?: number
+  images: Image[]
+  items: MenuItem[]
+}
+
+interface Menu {
+  id: number
+  name: string
+  type: string
+  collapse: number
+  sections: Section[]
+}
+
+export * from '.'
