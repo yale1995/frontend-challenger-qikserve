c710b3f66e50c0d3cd5fcea403ad021a847e659c
Yale Araujo <yale850@gmail.com>
2024-09-15 19:00:19 -0300

    refactor: internationalization by api locale

--- i18n.ts
+++ i18n.ts
@@ -1,7 +1,13 @@
 import { getRequestConfig } from 'next-intl/server'
 
 export default getRequestConfig(async () => {
-  const locale = 'en'
+  const response = await fetch(
+    'https://cdn-dev.preoday.com/challenge/venue/9',
+    { method: 'GET' },
+  )
+
+  const parsedResponse = await response.json()
+  const locale = parsedResponse.locale
 
   return {
     locale,
