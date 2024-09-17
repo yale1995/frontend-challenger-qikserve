08a4c771f57dff086692eacc621f7b856c51f68a
Yale Araujo <yale850@gmail.com>
2024-09-17 08:42:57 -0300

    feat: responsive menu navbar

---
+++ src/components/icons/hamburger-menu-icon-regular.tsx
@@ -0,0 +1,25 @@
+import { IconProps } from '.'
+
+export const HamburgerMenuIconRegular = (props: IconProps) => {
+  return (
+    <svg
+      {...props}
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_10633_656)">
+        <rect width="16" height="2" rx="1" />
+        <rect y="7" width="16" height="2" rx="1" />
+        <rect y="14" width="16" height="2" rx="1" />
+      </g>
+      <defs>
+        <clipPath id="clip0_10633_656">
+          <rect width="16" height="16" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,25 @@
 'use client'
 
+import { useMediaQuery } from '@/hooks/use-media-query'
 import { useTheme } from '@/hooks/use-theme'
 import { useTranslations } from 'next-intl'
 import { usePathname } from 'next/navigation'
 import { CSSProperties } from 'react'
+import { HamburgerMenuIconRegular } from '@/components/icons'
 
 export const Navbar = () => {
   const pathname = usePathname()
   const { theme } = useTheme()
   const t = useTranslations()
 
+  const isMobile = useMediaQuery('(max-width: 766px)')
+
   const navStyle: CSSProperties = {
     height: '3.25rem',
     background: theme.navBackgroundColour,
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.16)',
+    display: 'flex',
+    alignItems: 'center',
   }
 
   const listItemsStyle: CSSProperties = {
@@ -34,19 +40,68 @@ export const Navbar = () => {
     textAlign: 'center',
   })
 
+  const buttonMenuStyles: CSSProperties = {
+    width: '1.75rem',
+    height: '1.75rem',
+    background: 'transparent',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    border: '0',
+    outline: '0',
+
+    marginLeft: 'auto',
+    gridColumn: '3',
+  }
+
+  const iconButtonStyles: CSSProperties = {
+    color: theme.backgroundColour,
+    width: '1rem',
+    height: '1rem',
+  }
+
+  const contentContainerMobileStyles: CSSProperties = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '0 1rem',
+  }
+
+  const navHeadingStyles: CSSProperties = {
+    fontSize: '1.25rem',
+    fontWeight: '500',
+    color: theme.backgroundColour,
+    gridColumn: '2',
+    textAlign: 'center',
+  }
+
   return (
     <nav style={navStyle}>
-      <div style={listItemsStyle}>
-        <a href="/" style={itemStyle('/menu')}>
-          {t('navbar.menu')}
-        </a>
-        <a href="/sign-in" style={itemStyle('/sign-in')}>
-          {t('navbar.sign-in')}
-        </a>
-        <a href="/contact" style={itemStyle('/contact')}>
-          {t('navbar.contact')}
-        </a>
-      </div>
+      {!isMobile ? (
+        <div style={listItemsStyle}>
+          <a href="/" style={itemStyle('/menu')}>
+            {t('navbar.menu')}
+          </a>
+          <a href="/sign-in" style={itemStyle('/sign-in')}>
+            {t('navbar.sign-in')}
+          </a>
+          <a href="/contact" style={itemStyle('/contact')}>
+            {t('navbar.contact')}
+          </a>
+        </div>
+      ) : (
+        <div style={contentContainerMobileStyles}>
+          <h3 style={navHeadingStyles}>Menu</h3>
+
+          <button style={buttonMenuStyles}>
+            <HamburgerMenuIconRegular style={iconButtonStyles} />
+          </button>
+        </div>
+      )}
     </nav>
   )
 }
